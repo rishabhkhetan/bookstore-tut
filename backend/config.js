08cbee4e48bcd322d1a24ad54afadc4f713f34b3
rishabhkhetan <rishabh.khetan@outlook.com>
2024-03-10 15:36:06 +0530

    My MERN Bookstore app

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://root:root@books-store-mern.ct5aw1d.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+;
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
